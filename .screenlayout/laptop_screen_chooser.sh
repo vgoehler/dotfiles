af11fd886e780d80c58008338a4801b3c48267b6
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2019-08-13 08:57:08 +0200

    bugfix, subscripts started from wrong directory

--- .screenlayout/laptop_screen_chooser.sh
+++ .screenlayout/laptop_screen_chooser.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MULTI_SCREENS=$(xrandr | pcregrep -M "DP-2-2 connected(\n|.)*DP-2-3 connected")
 
 if [ -z "$MULTI_SCREENS" ]
 then
     # single screen
-    ./monitorSetupSingleLaptop.sh
+    $DIR/monitorSetupSingleLaptop.sh
 else
-    ./monitorSetupDockingStation.sh
+    $DIR/monitorSetupDockingStation.sh
 fi
