5dac2069f741487892918a52945b2dde4ff27af2
Volker Goehler <volker.goehler.zug@gmail.com>
2019-12-17 08:57:14 +0100

    detached okular, libreoffice start script from terminal as well

--- .config/fish/functions/libreoffice.fish
+++ .config/fish/functions/libreoffice.fish
@@ -1,6 +1,6 @@
 # shadowing libreoffice as it produces error, and I don't like the foreground blocking behaviour
 function libreoffice
     echo "Running libreoffice from fish script; Suppressing Errors"
-    /snap/bin/libreoffice 2>/dev/null $argv &
+    nohup /snap/bin/libreoffice 2>/dev/null $argv &
 end
 
