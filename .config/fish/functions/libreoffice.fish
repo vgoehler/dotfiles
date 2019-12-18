b7355a01de4d838e5520f38f755cfceca60a1d3e
Volker Goehler <volker.goehler.zug@gmail.com>
2019-12-18 10:11:24 +0100

    suppressing std out so no nohup.out files get generated

--- .config/fish/functions/libreoffice.fish
+++ .config/fish/functions/libreoffice.fish
@@ -1,6 +1,6 @@
 # shadowing libreoffice as it produces error, and I don't like the foreground blocking behaviour
 function libreoffice
-    echo "Running libreoffice from fish script; Suppressing Errors"
-    nohup /snap/bin/libreoffice 2>/dev/null $argv &
+    echo "Running libreoffice from fish script; Suppressing Errors, Suppressing Std Out, detaching"
+    nohup /snap/bin/libreoffice 2>/dev/null 1>/dev/null $argv &
 end
 
