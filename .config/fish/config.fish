a983d2ea31b3a456ab748c4906612fb0094d432f
Volker Goehler <volker.goehler.zug@gmail.com>
2020-04-08 16:38:21 +0200

    path for npm added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,5 @@ set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
 set -x VIRTUALENVWRAPPER_PYTHON /usr/bin/python3
 set -x WORKON_HOME $HOME/.virtualenvs
 bash /usr/local/bin/virtualenvwrapper.sh
+
+set PATH $HOME/.npm-global/bin $PATH
