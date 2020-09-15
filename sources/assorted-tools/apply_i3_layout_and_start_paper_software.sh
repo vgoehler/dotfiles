2910304ec251e06ffdc09c99ad7a5b727dd24ec3
Volker Goehler <volker.goehler.zug@gmail.com>
2020-09-15 14:16:52 +0200

    changed from mendeley desktop to mendeley reference manager

--- sources/assorted-tools/apply_i3_layout_and_start_paper_software.sh
+++ sources/assorted-tools/apply_i3_layout_and_start_paper_software.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 i3-msg "append_layout ~/.config/i3/workspace_reading.json"
 # And finally we fill the containers with the programs they had
-(mendeleydesktop&)
+(~/Software/AppImages/mendeley-reference-manager.AppImage &)
 (polar-bookshelf &)
