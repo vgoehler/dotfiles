ecef9f4844326a4db6464cd3fd4340579a29ec17
Volker Goehler <volker.goehler.zug@gmail.com>
2021-07-02 14:13:57 +0200

    feh is now called after monitor setup changes

--- .screenlayout/laptop_screen_chooser.sh
+++ .screenlayout/laptop_screen_chooser.sh
@@ -7,7 +7,9 @@ HOME_OFFICE=$(xrandr | pcregrep -M "DP-2-1 connected(\n|.)*DP-2-2 connected")
 if [ -n "$MULTI_SCREENS" ]
 then
     # office setup: 1 big screen, left 1 small screen, further left laptop screen
-    $DIR/monitorSetupDockingStationPrimaryBig.sh
+    # changed this as Ultra HD resolution is not ideal on xorg, wayland change?
+    # office setup: 1 screen, left another screen, further left laptop screen
+    $DIR/monitorSetupDockingStation.sh
 elif [ -n "$HOME_OFFICE" ]
 then
     # home office setup; 2 small screens, laptop off
