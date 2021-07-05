571500a229a6881daf0c4ef6b6c7ec68e0906110
Volker Goehler <volker.goehler.zug@gmail.com>
2021-07-05 14:06:07 +0200

    change reading workspace a bit

--- sources/assorted-tools/apply_i3_layout_and_start_paper_software.sh
+++ sources/assorted-tools/apply_i3_layout_and_start_paper_software.sh
@@ -2,4 +2,4 @@
 i3-msg "append_layout ~/.config/i3/workspace_reading.json"
 # And finally we fill the containers with the programs they had
 (~/Software/AppImages/mendeley-reference-manager.AppImage &)
-(polar-bookshelf &)
+(/usr/bin/mendeleydesktop &)
