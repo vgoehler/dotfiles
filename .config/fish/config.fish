81ae73dfc2f242a09ea5b53be1cdc054db2d2f8a
Volker Goehler <volker.goehler.zug@gmail.com>
2021-10-01 14:03:52 +0200

    additional java

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
+# set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
+set -x JAVA_HOME /usr/lib/jvm/bellsoft-java8-full-amd64
 
 set -x VIRTUALENVWRAPPER_PYTHON /usr/bin/python3
 set -x WORKON_HOME $HOME/.virtualenvs
