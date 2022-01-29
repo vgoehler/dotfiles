72f09ad6f7cc744080a2d666f5955c90f76dbc76
Volker Goehler <volker.goehler.zug@gmail.com>
2022-01-30 00:14:59 +0100

    okular change

--- .config/fish/functions/okular.fish
+++ .config/fish/functions/okular.fish
@@ -1,5 +1,5 @@
 # shadowing okular as it produces error, and I don't like the foreground blocking behaviour
 function okular
     echo "Running okular from fish script; Suppressing Errors, Suppressing Std Out, detaching"
-    nohup /usr/bin/okular 2>/dev/null 1>/dev/null $argv &
+    nohup /snap/bin/okular 2>/dev/null 1>/dev/null $argv &
 end
