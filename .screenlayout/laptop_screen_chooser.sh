b9d327c2f44790a8a2e21d5ddfeabd4dcac21f6f
Volker Goehler <volker.goehler.zug@gmail.com>
2022-03-17 11:11:12 +0100

    screen configuration Home Office done

--- .screenlayout/laptop_screen_chooser.sh
+++ .screenlayout/laptop_screen_chooser.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-exit -1
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-MULTI_SCREENS=$(xrandr | pcregrep -M "DP-2-2 connected(\n|.)*DP-2-3 connected")
-HOME_OFFICE=$(xrandr | pcregrep -M "DP-2-1 connected(\n|.)*DP-2-2 connected")
+MULTI_SCREENS=$(xrandr | pcregrep -M "DP-2-2 connected(\n|.)*DP-2-3 connected") #TODO needs to change
+HOME_OFFICE=$(xrandr | pcregrep -M "DVI-I-2-1 connected(\n|.)*DVI-I-3-2 connected")
+LAPTOP_SCREEN_INACTIVE=$(xrandr | pcregrep -M "eDP-1-0 connected \(")
 
 if [ -n "$MULTI_SCREENS" ]
 then
@@ -12,10 +11,18 @@ then
     # changed this as Ultra HD resolution is not ideal on xorg, wayland change?
     # office setup: 1 screen, left another screen, further left laptop screen
     $DIR/monitorSetupDockingStation.sh
+    # TODO needs switch between closed/opened laptop
 elif [ -n "$HOME_OFFICE" ]
 then
-    # home office setup; 2 small screens, laptop off
-    $DIR/monitorSetupHomeOffice_closedLaptop.sh
+    # home office setup
+    if [ -n "$LAPTOP_SCREEN_INACTIVE" ]
+    then
+        # laptop screen is active
+        $DIR/monitorSetupHomeOffice.sh
+    else
+        # 3 screens, laptop off
+        $DIR/monitorSetupHomeOffice_closedLaptop.sh
+    fi
 else
     # single screen
     $DIR/monitorSetupSingleLaptop.sh
