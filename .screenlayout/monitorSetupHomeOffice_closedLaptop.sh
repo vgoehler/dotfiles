e316cb26fef7b2b997a5005883ec99fc39d5db0e
Volker Goehler <volker.goehler.zug@gmail.com>
2022-10-14 11:17:23 +0200

    removed third monitor

--- .screenlayout/monitorSetupHomeOffice_closedLaptop.sh
+++ .screenlayout/monitorSetupHomeOffice_closedLaptop.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-xrandr --output DP-0 --primary --mode 1920x1080 --pos 320x1000 --rotate normal \
+xrandr --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
     --output DP-1 --off \
     --output HDMI-0 --off \
     --output DP-2 --off \
     --output DVI-I-5-4 --off \
     --output DVI-I-4-3 --off \
-    --output DVI-I-3-2 --mode 1920x1080 --pos 320x0 --rotate normal \
+    --output DVI-I-3-2 --off \
     --output eDP-1-0 --off \
-    --output DVI-I-2-1 --mode 1920x1080 --pos 2240x1000 --rotate normal
+    --output DVI-I-2-1 --mode 1920x1080 --pos 1920x0 --rotate normal
